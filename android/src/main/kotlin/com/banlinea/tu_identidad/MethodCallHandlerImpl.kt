7fe3c08aa9cef925be28fe61403f40406dd74c14
Cristian garcia <cristian.garcia@pegasi.io>
2022-01-20 15:53:13 -0500

    add application

--- android/src/main/kotlin/com/banlinea/tu_identidad/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/banlinea/tu_identidad/MethodCallHandlerImpl.kt
@@ -162,7 +162,7 @@ class MethodCallHandlerImpl: MethodChannel.MethodCallHandler, ActivityAware , Ac
 
              //   val bitmap: Bitmap = MediaStore.Images.Media.getBitmap( act!!.contentResolver, (extras.getParcelable("inebPath") as Uri))
               //  result["sdfasdfasdf"] = bitmap
-                contentResolver
+                activity!!.contentResolver
 
                 mResult!!.success(result)
                 true
