c45676bdd738710bb896cbd2e7240405e93684a2
Cristian garcia <cristian.garcia@pegasi.io>
2022-01-20 14:53:00 -0500

    add application

--- android/src/main/kotlin/com/banlinea/tu_identidad/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/banlinea/tu_identidad/MethodCallHandlerImpl.kt
@@ -20,16 +20,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 
 import android.app.Activity
-import android.content.Intent
 import android.provider.ContactsContract
-import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
